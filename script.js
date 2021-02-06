cfb205edbc9ea292313a4736914f3ea384ce2f6c
ssmeds <stina_smeds@hotmail.com>
2021-02-06 13:34:51 +0100

    finished the flair and adding comments

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
+// Create element and give it a classname
 let div = document.createElement("div");
+div.className = "clockDiv";
 
-
+// Append the div to the body
 document.body.appendChild(div);
 
-setInterval(function() {
+// Set interval for the clock-function to run
+setInterval(function () {
     clock();
-    console.log("Klockan ska uppdatera sig");
-}, 1000);
+}, 1000); // The interval is 1000 milliseconds = 1 second
 
+// Function for displaying the time
 function clock() {
     div.innerHTML = "";
-let today = new Date();
-
-div.insertAdjacentHTML("afterbegin", `${today.getHours()} : ${today.getMinutes()} : ${today.getSeconds()}`);
+    // Declaring that "today" is a new method which we will later use
+    let today = new Date();
+    // Displaying the hours, minutes and seconds
+    div.innerHTML = `${today.getHours()} : ${today.getMinutes()} : ${today.getSeconds()}`;
 };
-// Date() ger oss ett inbyggt objekt i Javacript med vilket vi kan hantera datum på olika sätt. Du kan behöva det om du exempelvis vill spara tiden för när en order är skickad i en webshop. Eller om vi vill visa hur lång tid det är kvar till en deadline till en uppgift.
-
-// I denna övningen vill jag att du skall skapa en digital klocka.
-
-// Du skall bara visa timmar:minuter:sekunder Välj ut ett typsnitt från google fonts som ser ut som en gammal retro digitalklocka. Du kommer behöva använda Date() samt setInterval() för att slutföra uppgiften.
